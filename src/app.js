257578555f54706c4b9f189c6bcc9d30a55cdf2d
Sergio García <sergio4geeks@gmail.com>
2025-04-08 19:40:21 +0000

    generador de excusas finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let frase ="";
+
+  function aleatorio(arr){
+    return Math.floor(Math.random() * arr.length);
+  }
+  
+    frase += `${who[aleatorio(who)]} ${action[aleatorio(action)]} ${what[aleatorio(what)]} ${when[aleatorio(when)]}`;
+  console.log(frase);
+  document.getElementById("excuse").innerHTML = frase;
 };
